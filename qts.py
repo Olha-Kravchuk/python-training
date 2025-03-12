2449959c2058637f49863f218dcdf9bd3e70bd9e
Olha-Kravchuk <kravchukolya0@gmail.com>
2025-03-12 11:38:23 +0100

    3

--- qts.py
+++ qts.py
@@ -1,3 +1,11 @@
 import sys
-from PyQts.Qtwidgets omport QAppkication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    app = QApplication(sys.argv)
 
